58970dcdfdce845a15b21dcc8fc2577348422219
Jaime Bernáldez <242479426+jaimebr2002-boop@users.noreply.github.com>
2026-09-24 11:03:23 +0200

    feat(portfolio): add Segurma and Bexie logos

--- src/content/clients.ts
+++ src/content/clients.ts
@@ -3,6 +3,8 @@ import hyundaiAsturdaiLogo from '../assets/clients/hyundai-asturdai.png';
 import elCastoOviedoLogo from '../assets/clients/el-casto-oviedo.png';
 import mambaNegraLogo from '../assets/clients/mamba-negra.png';
 import naviserLogo from '../assets/clients/naviser.png';
+import segurmaLogo from '../assets/clients/segurma.png';
+import bexieLogo from '../assets/clients/bexie.png';
 
 export interface Client {
   name: string;
@@ -17,4 +19,6 @@ export const clients: Client[] = [
   { name: 'El Casto Oviedo', logo: elCastoOviedoLogo },
   { name: 'Mamba Negra Food & Drink', logo: mambaNegraLogo },
   { name: 'Grupo Naviser', logo: naviserLogo },
+  { name: 'Segurma', logo: segurmaLogo },
+  { name: 'Bexie Energy', logo: bexieLogo },
 ];
